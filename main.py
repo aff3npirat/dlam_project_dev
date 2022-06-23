5a331dbddc313175ddb34a1f463c7285196ad83c
Jurek <jurek.elliesen@web.de>
2022-06-23 13:24:53 +0200

    Add todos

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import timm
+import torch.nn as nn
 
+
+model = timm.create_model("resnet50", pretrained=True)
+
+for m in model.modules():
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        print(f"{m} -> {m.weight.shape}")
+
+# TODO train resnet on cifar10
+# TODO random selection
+# TODO folder structure
