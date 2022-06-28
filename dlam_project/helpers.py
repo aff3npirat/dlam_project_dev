ee710e9eecffc5bd32f0cca1b2dabd1f5f4cd931
Jurek <jurek.elliesen@web.de>
2022-06-28 20:29:28 +0200

    Add helpers

---
+++ dlam_project/helpers.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import math
+
+
+def count_params(model, layers=(nn.Linear, nn.Conv2d, nn.Conv1d, nn.Conv3d)):
+    """
+    Counts number of trainable layers and parameters.
+    """
+    num_params = 0
+    num_layers = 0
+    for m in model.modules():
+        if isinstance(m, layers):
+            num_layers += 1
+            num_params += math.prod(m.weight.shape)
+            if m.bias is not None:
+                num_params += math.prod(m.bias.shape)
+    return (num_layers, num_params)
+
+
+def get_ith_trainable(model, i, layers=(nn.Linear, nn.Conv2d, nn.Conv1d, nn.Conv3d)):
+    """
+    Returns index of ith trainable layer.
+    """
+    for idx, m in enumerate(model.modules()):
+        if isinstance(m, layers):
+            i -= 1
+        if i == 0:
+            return idx
+
+
+def sign(x):
+    if x >= 0:
+        return 1.0
+    return -1.0
