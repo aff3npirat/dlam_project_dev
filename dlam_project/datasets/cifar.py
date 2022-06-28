bab294f371a485f0a969743cb68b96969a72ae1a
Jurek <jurek.elliesen@web.de>
2022-06-28 14:11:13 +0200

    Moved directory

---
+++ dlam_project/datasets/cifar.py
@@ -0,0 +1,11 @@
+from torchvision import datasets, transforms
+
+
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# 32 x 32 x 3 pixels
+cifar10 = datasets.CIFAR10
+train_set = cifar10("./store/datasets/data/cifar10", download=True, train=True, transform=transform)
+test_set = cifar10("./store/datasets/data/cifar10", download=True, train=False, transform=transform)
