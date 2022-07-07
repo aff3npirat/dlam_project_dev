ccb5622841586f4fffea7d2d37b65a9762affb9a
Jurek <jurek.elliesen@web.de>
2022-07-07 17:28:13 +0200

    Change FileExistsError behaviour

--- utils/utils.py
+++ utils/utils.py
@@ -2,13 +2,21 @@ import numpy as np
 import random
 import torch
 import os
+from datetime import datetime
 
 from dlam_project.evaluation import eval_model
 
 
 
-def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None):
-    os.makedirs(save_to, exist_ok=True)
+def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, raise_exc=True):
+    try:
+        os.makedirs(save_to, exist_ok=False)
+    except FileExistsError as exc:
+        if raise_exc:
+            raise exc
+        else:
+            save_to = f"{save_to}_{datetime.now().strftime('%m-%d_%H-%M-%S')}"
+            test(change_model_fc, save_to, model, eval_fc, device, raise_exc)
     
     if model is None:
         model = torch.load("./saves/base/model.pt")
