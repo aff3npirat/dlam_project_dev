a65ab9c1be18b77dac707f1dcf28a46f5415887d
Jurek <jurek.elliesen@web.de>
2022-07-07 17:36:50 +0200

    Change FileExistsError behaviour
    duplicate can be passed either {"skip", "raise", "rename"} which will skip run, end experiment or rename
    run when run directory already exists.

--- utils/utils.py
+++ utils/utils.py
@@ -8,22 +8,26 @@ from dlam_project.evaluation import eval_model
 
 
 
-def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, raise_exc=True):
+def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, duplicate="skip"):
+    if model is None:
+        model = torch.load("./saves/base/model.pt")
+
     try:
         os.makedirs(save_to, exist_ok=False)
     except FileExistsError as exc:
-        if raise_exc:
+        if duplicate == "raise":
             raise exc
-        else:
+        elif duplicate == "skip":
+            return change_model_fc(model)
+        elif duplicate == "rename":
             save_to = f"{save_to}_{datetime.now().strftime('%m-%d_%H-%M-%S')}"
-            test(change_model_fc, save_to, model, eval_fc, device, raise_exc)
-    
-    if model is None:
-        model = torch.load("./saves/base/model.pt")
-    
+            test(change_model_fc, save_to, model, eval_fc, device, duplicate)
+
     model = change_model_fc(model)
 
     eval_fc(model, save_to, device=device)
+    
+    return model
 
 
 def setup():
