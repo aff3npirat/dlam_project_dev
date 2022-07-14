589d8ae55c612d809c372e5458501381c2e91918
Jurek <jurek.elliesen@web.de>
2022-07-14 16:33:53 +0200

    Refactoring

--- utils/utils.py
+++ utils/utils.py
@@ -8,7 +8,7 @@ from dlam_project.evaluation import eval_model
 
 
 
-def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, duplicate="skip"):
+def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, handle_duplicate="skip"):
     """
     Args:
         duplicate: one of {"skip", "raise", "rename"}.
@@ -19,13 +19,13 @@ def test(change_model_fc, save_to, model=None, eval_fc=eval_model, device=None, 
     try:
         os.makedirs(save_to, exist_ok=False)
     except FileExistsError as exc:
-        if duplicate == "raise":
+        if handle_duplicate == "raise":
             raise exc
-        elif duplicate == "skip":
+        elif handle_duplicate == "skip":
             return change_model_fc(model)
-        elif duplicate == "rename":
+        elif handle_duplicate == "rename":
             save_to = f"{save_to}_{datetime.now().strftime('%m-%d_%H-%M-%S')}"
-            test(change_model_fc, save_to, model, eval_fc, device, duplicate)
+            test(change_model_fc, save_to, model, eval_fc, device, handle_duplicate)
 
     model = change_model_fc(model)
 
